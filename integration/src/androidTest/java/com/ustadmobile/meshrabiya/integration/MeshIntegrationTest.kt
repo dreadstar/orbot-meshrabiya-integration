94de631f55dc16d906aa6fadb4b9d7ab3287905e
John Bobo <8541246+JohnBobo@users.noreply.github.com>
2025-07-01 15:13:39 -0400

    still running here

--- integration/src/androidTest/java/com/ustadmobile/meshrabiya/integration/MeshIntegrationTest.kt
+++ integration/src/androidTest/java/com/ustadmobile/meshrabiya/integration/MeshIntegrationTest.kt
@@ -9,14 +9,10 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.ustadmobile.meshrabiya.beta.BetaTestLogger
 import com.ustadmobile.meshrabiya.beta.LogLevel
 import com.ustadmobile.meshrabiya.mmcp.MmcpOriginatorMessage
-import com.ustadmobile.meshrabiya.vnet.MeshRole
 import com.ustadmobile.meshrabiya.vnet.MeshRoleManager
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import kotlin.test.assertEquals
-import kotlin.test.assertNotNull
-import kotlin.test.assertTrue
 
 @RunWith(AndroidJUnit4::class)
 class MeshIntegrationTest {
